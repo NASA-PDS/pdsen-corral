4476eeb151779843eb2d009e089d8e0ab40b3100
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2020-05-27 19:25:09 -0700

    not encode the url to the different resources from the summary

--- pdsen_corral/cattle_head.py
+++ pdsen_corral/cattle_head.py
@@ -47,7 +47,7 @@ class CattleHead():
 
     def _get_cell(self, function):
         link_func = eval(f'self._get_{function}_link()')
-        return f'[![{function}]({self._icon_dict[function]})]({requests.utils.quote(link_func)} "{function}")'
+        return f'[![{function}]({self._icon_dict[function]})]({link_func} "{function}")'
 
     def _get_download_link(self):
         return f'https://github.com/NASA-PDS/{self._repo_name}/releases/tag/{self._version}'
